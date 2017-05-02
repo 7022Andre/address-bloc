8eb21e089fda7a322b3d75f83de454f6aed3c641
7022Andre <7022Andre@users.noreply.github.com>
2017-05-02 13:33:12 -0700

    Checkpoint 6 complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -3,6 +3,12 @@ require_relative "../models/address_book"
 RSpec.describe AddressBook do
 	let(:book) { AddressBook.new }
 
+	def check_entry(entry, expected_name, expected_number, expected_email)
+		expect(entry.name).to eq expected_name
+		expect(entry.phone_number).to eq expected_number
+		expect(entry.email).to eq expected_email
+	end
+
 	describe "attributes" do
 		it "responds to entries" do
 			expect(book).to respond_to(:entries)
@@ -29,6 +35,45 @@ RSpec.describe AddressBook do
 			expect(new_entry.name).to eq("Peter Parker")
 			expect(new_entry.phone_number).to eq("800-888-8888")
 			expect(new_entry.email).to eq("peter@parker.com")
+		end
+	end
+
+	describe "#import_from_csv" do
+		it "imports the correct number of entries" do
+			book.import_from_csv("entries.csv")
+			book_size = book.entries.size
+
+			expect(book_size).to eq(5)
+		end
+
+		it "imports first entry" do
+			book.import_from_csv("entries.csv")
+			entry_one = book.entries[0]
+			check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+		end
+
+		it "imports second entry" do
+			book.import_from_csv("entries.csv")
+			entry_two = book.entries[1]
+			check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+		end
+
+		it "imports third entry" do
+			book.import_from_csv("entries.csv")
+			entry_three = book.entries[2]
+			check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+		end
+
+		it "imports fourth entry" do
+			book.import_from_csv("entries.csv")
+			entry_four = book.entries[3]
+			check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+		end
+
+		it "imports fifth entry" do
+			book.import_from_csv("entries.csv")
+			entry_five = book.entries[4]
+			check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
 		end
 	end
 end
