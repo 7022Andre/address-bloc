12082ce629ebb7d1cef872989911f663d2bcf692
7022Andre <7022Andre@users.noreply.github.com>
2017-05-02 09:06:28 -0700

    Checkpoint 5 complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -7,6 +7,44 @@ class MenuController
 		@address_book = AddressBook.new
 	end
 
+	def main_menu
+		puts "Main Menu - #{address_book.entries.count} entries"
+		puts "1 - View all entries"
+		puts "2 - Create an entry"
+		puts "3 - Search for an entry"
+		puts "4 - Import entries from a CSV"
+		puts "5 - Exit"
+		print "Enter your selection: "
+
+		selection = gets.to_i
+		
+		case selection
+			when 1
+				system "clear"
+				view_all_entries
+				main_menu
+			when 2
+				system "clear"
+				create_entry
+				main_menu
+			when 3
+				system "clear"
+				search_entries
+				main_menu
+			when 4
+				system "clear"
+				read_csv
+				main_menu
+			when 5
+				puts "Good-bye!"
+				exit(0)
+			else
+				system "clear"
+				puts "Sorry, that is not a valid input"
+				main_menu
+		end
+	end
+
 	def view_all_entries
 		address_book.entries.each do |entry|
 			system "clear"
@@ -61,45 +99,5 @@ class MenuController
 				puts "#{selection} is not a valid input"
 				entry_submenu(entry)
 		end
-	end
-
-	def main_menu
-		puts "Main Menu - #{address_book.entries.count} entries"
-		puts "1 - View all entries"
-		puts "2 - Create an entry"
-		puts "3 - Search for an entry"
-		puts "4 - Import entries from a CSV"
-		puts "5 - Exit"
-		print "Enter your selection: "
-
-		selection = gets.to_i
-		
-		case selection
-			when 1
-				system "clear"
-				view_all_entries
-				main_menu
-			when 2
-				system "clear"
-				create_entry
-				main_menu
-			when 3
-				system "clear"
-				search_entries
-				main_menu
-			when 4
-				system "clear"
-				read_csv
-				main_menu
-			when 5
-				puts "Good-bye!"
-				exit(0)
-			else
-				system "clear"
-				puts "Sorry, that is not a valid input"
-				main_menu
-		end
-
-
 	end
 end
